5c11e9c4aae06379607cae1f63954cbb64570db6
nriin-rizeki <ipandani2505@gmail.com>
2023-01-17 23:21:26 +0700

    chore: disable eslint rule in all release.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '{apps,libs,tools}/**/*.{ts,js,json,md,html,css,scss}': [
     'nx affected --target lint --uncommitted --fix true',
-    'nx affected --target test --uncommitted',
+    // 'nx affected --target test --uncommitted',
     'nx format:write --uncommitted',
   ],
 };
