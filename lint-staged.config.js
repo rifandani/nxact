52d71a302f7cdd8b9b3ec8744ac00c71656bf252
nriin-rizeki <ipandani2505@gmail.com>
2023-01-18 13:03:03 +0700

    fix: delete workspace-lint run command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   '{apps,libs,tools}/**/*.{ts,js,json,md,html,css,scss}': [
-    'nx affected --target lint --uncommitted --fix true',
+    // 'nx affected --target lint --uncommitted --fix true',
     // 'nx affected --target test --uncommitted',
     'nx format:write --uncommitted',
   ],
