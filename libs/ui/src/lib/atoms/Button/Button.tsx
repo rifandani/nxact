0e1ba5e7c59baebf2e36b6267dbc0e519042d4d6
nriin-rizeki <ipandani2505@gmail.com>
2023-01-19 20:44:30 +0700

    feat(ui): new Button

---
+++ libs/ui/src/lib/atoms/Button/Button.tsx
@@ -0,0 +1,150 @@
+import { ColorVariant, Size } from 'libs/ui/src/types/common.type';
+import { cn } from 'libs/ui/src/utils/cn.util';
+import React, { cloneElement } from 'react';
+import '../../../index.css';
+
+// #region INTERFACES
+type ButtonType = 'solid' | 'outlined' | 'text';
+
+type IconProps = React.ComponentProps<'div'> & {
+  size: Size;
+};
+
+type BaseButtonProps = React.ComponentProps<'button'> & {
+  size: Size;
+  buttonType: ButtonType;
+  variant: ColorVariant;
+  rounded: boolean;
+};
+
+type ButtonProps = React.ComponentProps<'button'> & {
+  testId?: string;
+  size?: Size;
+  buttonType?: ButtonType;
+  variant?: ColorVariant;
+  rounded?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+};
+// #endregion
+
+const buttonSizeClasses = {
+  small: 'py-1.5 text-sm rounded text-red-500',
+  medium: 'py-[9px] text-base rounded-md',
+  large: 'py-3 text-lg rounded-md',
+};
+const buttonTypeClasses = {
+  solid:
+    'text-white disabled:border-gray-100 disabled:text-gray-400 disabled:bg-gray-100',
+  outlined:
+    'bg-opacity-0 hover:bg-opacity-20 disabled:border-gray-300 disabled:text-gray-300',
+  text: 'disabled:text-gray-300 border-transparent bg-transparent',
+};
+const solidButtonVariantClasses = {
+  primary:
+    'border-primary bg-primary hover:bg-primary-dark hover:border-primary-dark',
+  secondary:
+    'border-secondary bg-secondary hover:bg-secondary-dark hover:border-secondary-dark',
+  error: 'border-error bg-error hover:bg-error-dark hover:border-error-dark',
+  warning:
+    'border-warning bg-warning hover:bg-warning-dark hover:border-warning-dark',
+  info: 'border-info bg-info hover:bg-info-dark hover:border-info-dark',
+  success:
+    'border-success bg-success hover:bg-success-dark hover:border-success-dark',
+};
+const outlinedButtonVariantClasses = {
+  primary: 'text-primary border-primary bg-primary',
+  secondary: 'text-secondary border-secondary bg-secondary',
+  error: 'text-error border-error bg-error',
+  warning: 'text-warning border-warning bg-warning',
+  info: 'text-info border-info bg-info',
+  success: 'text-success border-success bg-success',
+};
+const textButtonVariantClasses = {
+  primary: 'text-primary hover:text-primary-dark',
+  secondary: 'text-secondary hover:text-secondary-dark',
+  error: 'text-error hover:text-error-dark',
+  warning: 'text-warning hover:text-warning-dark',
+  info: 'text-info hover:text-info-dark',
+  success: 'text-success hover:text-success-dark',
+};
+
+function BaseButton({
+  size,
+  buttonType,
+  variant,
+  rounded,
+  children,
+}: BaseButtonProps) {
+  return (
+    <button
+      className={cn(
+        'inline-flex cursor-pointer items-center justify-center space-x-3 border border-solid px-4 font-semibold leading-none duration-200',
+        'disabled:pointer-events-none disabled:cursor-default',
+        buttonSizeClasses[size],
+        buttonType === 'solid' && solidButtonVariantClasses[variant],
+        buttonType === 'outlined' && outlinedButtonVariantClasses[variant],
+        buttonType === 'text' && textButtonVariantClasses[variant],
+        buttonTypeClasses[buttonType],
+        !rounded && 'rounded-none'
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+function Icon({ size, children }: IconProps) {
+  return (
+    <div
+      className={cn(
+        size === Size.small && 'h-5 w-5',
+        size === Size.medium && 'h-6 w-6',
+        size === Size.large && 'h-7 w-7'
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function Button({
+  testId = 'nxact-ui-button',
+  size = Size.medium,
+  buttonType = 'solid',
+  variant = ColorVariant.primary,
+  rounded = true,
+  leftIcon,
+  rightIcon,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <BaseButton
+      data-testid={testId}
+      size={size}
+      buttonType={buttonType}
+      variant={variant}
+      rounded={rounded}
+      {...props}
+    >
+      {!!leftIcon && (
+        <Icon size={size}>
+          {cloneElement(leftIcon as React.ReactElement, {
+            width: '100%',
+            height: '100%',
+          })}
+        </Icon>
+      )}
+      {children}
+      {!!rightIcon && (
+        <Icon size={size}>
+          {cloneElement(rightIcon as React.ReactElement, {
+            width: '100%',
+            height: '100%',
+          })}
+        </Icon>
+      )}
+    </BaseButton>
+  );
+}
--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,5 +1,7 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const { blue, pink, red, yellow, green } = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,7 +13,61 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: blue[500],
+          light: blue[300],
+          lighter: blue[100],
+          dark: blue[700],
+          darker: blue[900],
+        },
+        secondary: {
+          DEFAULT: pink[500],
+          light: pink[300],
+          lighter: pink[100],
+          dark: pink[700],
+          darker: pink[900],
+        },
+        error: {
+          DEFAULT: red[500],
+          light: red[300],
+          lighter: red[100],
+          dark: red[700],
+          darker: red[900],
+        },
+        warning: {
+          DEFAULT: yellow[500],
+          light: yellow[300],
+          lighter: yellow[100],
+          dark: yellow[700],
+          darker: yellow[900],
+        },
+        info: {
+          DEFAULT: blue[500],
+          light: blue[300],
+          lighter: blue[100],
+          dark: blue[700],
+          darker: blue[900],
+        },
+        success: {
+          DEFAULT: green[500],
+          light: green[300],
+          lighter: green[100],
+          dark: green[700],
+          darker: green[900],
+        },
+      },
+      fontFamily: {
+        sans: ['Noto Sans JP', ...fontFamily.sans],
+        montserrat: ['Montserrat'],
+      },
+      screens: {
+        xl: '1280px',
+        '2xl': '2560px',
+        '3xl': '4500px',
+      },
+    },
   },
   plugins: [],
 };
