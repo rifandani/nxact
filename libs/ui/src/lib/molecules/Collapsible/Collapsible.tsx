a0b83a4035b1290a2c3661db851e9a67c4e53773
nriin-rizeki <ipandani2505@gmail.com>
2023-01-27 15:36:34 +0700

    fix(ui): move Collapsible lib to deps

--- libs/ui/src/lib/molecules/Collapsible/Collapsible.tsx
+++ libs/ui/src/lib/molecules/Collapsible/Collapsible.tsx
@@ -1,11 +1,9 @@
-import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
-
-import '../../../index.css';
-
-const Collapsible = CollapsiblePrimitive.Root;
-
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
-
-export { Collapsible, CollapsibleTrigger, CollapsibleContent };
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleContentImplProps,
+  CollapsibleContentProps,
+  CollapsibleProps,
+  CollapsibleTrigger,
+  CollapsibleTriggerProps,
+} from '@radix-ui/react-collapsible';
