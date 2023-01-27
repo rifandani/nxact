c7981376d03cce3164b3bce723d21879fb738bc9
nriin-rizeki <ipandani2505@gmail.com>
2023-01-27 16:24:42 +0700

    fix(ui): move Checkbox lib to deps

--- libs/ui/src/lib/atoms/Checkbox/Checkbox.tsx
+++ libs/ui/src/lib/atoms/Checkbox/Checkbox.tsx
@@ -1,4 +1,9 @@
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import {
+  CheckboxProps,
+  CheckedState,
+  Indicator,
+  Root,
+} from '@radix-ui/react-checkbox';
 import React from 'react';
 
 import { cn } from 'libs/ui/src/utils/cn.util';
@@ -6,10 +11,10 @@ import '../../../index.css';
 import { Check, Minus } from '../Icon/Icon';
 
 const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root>
 >(({ className, checked, ...props }, ref) => (
-  <CheckboxPrimitive.Root
+  <Root
     ref={ref}
     className={cn(
       'peer h-4 w-4 shrink-0 rounded-sm border border-slate-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
@@ -17,15 +22,13 @@ const Checkbox = React.forwardRef<
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn('flex items-center justify-center')}
-    >
+    <Indicator className={cn('flex items-center justify-center')}>
       {checked === 'indeterminate' && <Minus className="h-4 w-4" />}
       {checked === true && <Check className="h-4 w-4" />}
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+    </Indicator>
+  </Root>
 ));
 
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+Checkbox.displayName = Root.displayName;
 
-export { Checkbox };
+export { Checkbox, CheckboxProps, CheckedState };
