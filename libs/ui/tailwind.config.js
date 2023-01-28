f1b9348094936fc2bec045aa92fa9f476fb27eb4
nriin-rizeki <ipandani2505@gmail.com>
2023-01-28 19:38:19 +0700

    fix(ui): manual typing Accordion component

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -67,6 +67,20 @@ module.exports = {
         '2xl': '2560px',
         '3xl': '4500px',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [],
