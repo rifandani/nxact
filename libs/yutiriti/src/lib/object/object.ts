0a823bab6bd51f6effbddf22edbba9b0b116843d
nriin-rizeki <ipandani2505@gmail.com>
2023-03-12 20:27:22 +0700

    feat(yutiriti): add deepReadObject function

--- libs/yutiriti/src/lib/object/object.ts
+++ libs/yutiriti/src/lib/object/object.ts
@@ -599,3 +599,39 @@ export function fromPairs(tuples: ReadonlyArray<[string | number, unknown]>) {
 export function toPairs<T>(object: { [s: string]: T }): Array<[string, T]> {
   return Object.entries(object);
 }
+
+/**
+ * Safely access deep values in an object via a string path seperated by `.`
+ * This util is largely inspired by [dlv](https://github.com/developit/dlv/blob/master/index.js) and passes all its tests
+ *
+ * @param obj {Record<string, unknown>} - The object to parse
+ * @param path {string} - The path to search in the object
+ * @param [defaultValue] {unknown} -  A default value if the path doesn't exist in the object
+ *
+ * @returns {any} The value if found, the default provided value if set and not found, undefined otherwise
+ *
+ * @example
+ *
+ * ```js
+ * const obj = { a: { b : { c: 'hello' } } };
+ *
+ * const value = deepReadObject(obj, 'a.b.c');
+ * // => 'hello'
+ * const notFound = deepReadObject(obj, 'a.b.d');
+ * // => undefined
+ * const notFound = deepReadObject(obj, 'a.b.d', 'not found');
+ * // => 'not found'
+ * ```
+ */
+export const deepReadObject = <T = any>(
+  obj: Record<string, unknown>,
+  path: string,
+  defaultValue?: unknown
+): T => {
+  const value = path
+    .trim()
+    .split('.')
+    .reduce<any>((a, b) => (a ? a[b] : undefined), obj);
+
+  return value !== undefined ? value : defaultValue;
+};
